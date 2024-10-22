51b1b4528a7104d8caeace804aff3612f47e6440
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-10-22 14:05:12 +0200

    Auth data fetch

---
+++ src/lib/data/auth.ts
@@ -0,0 +1,10 @@
+import { routeApi } from "@/lib/router/router";
+import { fetchApi } from "@/lib/utils/fetch";
+
+export const fetchUser = async (): Promise<{
+    [key: string]: unknown;
+} | null> => {
+    return fetchApi(routeApi("api.user")).then(
+        async (response) => (await response.json())?.data ?? null
+    );
+};
