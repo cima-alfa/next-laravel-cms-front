3cafc7a1546fc3068ac78c36b953847fd02aeef1
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 09:31:30 +0100

    Change `fetchUser` to `fetchAuthenticated`

--- src/lib/middleware/authMiddleware.ts
+++ src/lib/middleware/authMiddleware.ts
@@ -1,8 +1,8 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { fetchUser } from "@/lib/data/auth";
-import { Route, route } from "@/lib/router/router";
-import { CustomMiddleware } from "@/lib/middleware/_chain";
-import { hasMiddleware } from "@/middleware";
+import { fetchAuthenticated } from "@/lib/data/auth";
+import { Route, link } from "@/lib/router/router";
+import { CustomMiddleware } from "@/lib/middleware";
+import { hasMiddleware } from "@/lib/middleware";
 
 export const authMiddleware = (
     currentRoute: Route | null,
@@ -14,10 +14,10 @@ export const authMiddleware = (
         response: NextResponse
     ) => {
         if (hasMiddleware(currentRoute, "front:auth")) {
-            const user = await fetchUser();
+            const authenticated = await fetchAuthenticated();
 
-            if (!user) {
-                return NextResponse.redirect(route("front.login", {}, true));
+            if (!authenticated) {
+                return NextResponse.redirect(link("front.login", {}, true));
             }
         }
 
--- src/lib/middleware/guestMiddleware.ts
+++ src/lib/middleware/guestMiddleware.ts
@@ -1,8 +1,8 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { fetchUser } from "@/lib/data/auth";
-import { Route, route } from "@/lib/router/router";
-import { CustomMiddleware } from "@/lib/middleware/_chain";
-import { hasMiddleware } from "@/middleware";
+import { fetchAuthenticated } from "@/lib/data/auth";
+import { Route, link } from "@/lib/router/router";
+import { CustomMiddleware } from "@/lib/middleware";
+import { hasMiddleware } from "@/lib/middleware";
 
 export const guestMiddleware = (
     currentRoute: Route | null,
@@ -14,11 +14,11 @@ export const guestMiddleware = (
         response: NextResponse
     ) => {
         if (hasMiddleware(currentRoute, "front:guest")) {
-            const user = await fetchUser();
+            const authenticated = await fetchAuthenticated();
 
-            if (user) {
+            if (authenticated) {
                 return NextResponse.redirect(
-                    route("front.cp.dashboard.index", {}, true)
+                    link("front.cp.dashboard.index", {}, true)
                 );
             }
         }
