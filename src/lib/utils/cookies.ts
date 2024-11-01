3e02eaa4c0471e3093ce8fd4e288fd312f0cdb66
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 10:18:27 +0100

    Move cookies functionality

--- src/lib/utils/cookies.ts
+++ src/lib/utils/cookies.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import {
+    RequestCookie,
+    ResponseCookie,
+} from "next/dist/compiled/@edge-runtime/cookies";
 import { cookies } from "next/headers";
 
 export async function getCookies(): Promise<RequestCookie[]>;
@@ -46,3 +49,45 @@ export const getCookie = async (name: string) => {
 };
 
 export const getCookieStore = async () => await cookies();
+
+export const setCookie = async (cookie: string) => {
+    const cookieStore = await cookies();
+
+    const cookieData = `${cookie};`.matchAll(/\s*(.+?)\s*(?:=\s*(.+?))?\s*;/g);
+
+    const cookieNameValue = cookieData.next();
+
+    const setCookie: { [key: string]: string } = {
+        name: cookieNameValue.value?.at(1) as string,
+        value: cookieNameValue.value?.at(2) as string,
+    };
+
+    cookieData.forEach((data) => {
+        // eslint-disable-next-line prefer-const
+        let [, key, value] = data;
+
+        switch (key.toLowerCase()) {
+            case "httponly":
+                key = "httpOnly";
+                break;
+
+            case "samesite":
+                key = "sameSite";
+                break;
+
+            case "max-age":
+                key = "maxAge";
+                break;
+        }
+
+        setCookie[key] = value ?? true;
+    });
+
+    cookieStore.set(setCookie as unknown as ResponseCookie);
+};
+
+export const setCookies = async (cookies: string[]) => {
+    cookies.forEach((cookie) => {
+        setCookie(cookie);
+    });
+};
