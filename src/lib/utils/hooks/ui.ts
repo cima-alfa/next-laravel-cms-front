28f88dff1bc557c64752cbb515f224f74659895b
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 09:40:23 +0100

    UI Utils

---
+++ src/lib/utils/hooks/ui.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState<{
+        [key: string]: number | undefined;
+    }>({
+        width: undefined,
+        height: undefined,
+    });
+
+    useEffect(() => {
+        async function handleResize() {
+            //await new Promise((resolve) => setTimeout(resolve, 250));
+
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowSize;
+};
