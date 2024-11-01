ba1cfc72f7f5d271da7393515924b6800147a634
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 09:34:07 +0100

    Auth functionality

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,10 +1,37 @@
 "use server";
 
-import { routeApi } from "@/lib/router/router";
-import { fetchApi } from "@/lib/utils/fetch";
+import { hasMiddleware } from "@/lib/middleware";
+import { getCurrentRoute, link, linkApi } from "@/lib/router/router";
+import { fetchApi, setCookies } from "@/lib/utils/fetch";
+import { redirect, RedirectType } from "next/navigation";
 
-export const login = async (state: void | undefined, formData: FormData) => {
-    //setErrors([]);
+export type AuthState = {
+    message: string;
+    errors?: { [key: string]: string[] };
+} | null;
+
+export const logout = async (pathname?: string) => {
+    await fetchApi(linkApi("logout"), { method: "POST" }, true).then(
+        async (response) => {
+            if (!response.ok) {
+                return;
+            }
+
+            await setCookies(response);
+
+            if (pathname !== undefined) {
+                const currentRoute = getCurrentRoute(pathname);
+
+                if (hasMiddleware(currentRoute, "front:auth")) {
+                    redirect(link("front.login"), RedirectType.replace);
+                }
+            }
+        }
+    );
+};
+
+export const login = async (prevState: AuthState, formData: FormData) => {
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = {
         login: formData?.get("login"),
@@ -16,14 +43,25 @@ export const login = async (state: void | undefined, formData: FormData) => {
         body: JSON.stringify(data),
     };
 
-    return fetchApi(routeApi("login"), options, true).then((response) => {
-        return { ok: response.ok, setCookie: response.headers.getSetCookie() };
-    });
+    return fetchApi(linkApi("login.store"), options, true).then(
+        async (response) => {
+            if (!response.ok) {
+                const data = await response.json();
+
+                return {
+                    message: data.message,
+                    errors: data.errors,
+                };
+            }
+
+            await setCookies(response);
+
+            redirect(link("front.cp.dashboard.index"), RedirectType.replace);
+        }
+    );
 };
 
-export const register = async (state: void | undefined, formData: FormData) => {
-    //setErrors([]);
-
+export const register = async (prevState: AuthState, formData: FormData) => {
     const data = {
         username: formData?.get("username"),
         email: formData?.get("email"),
@@ -36,5 +74,20 @@ export const register = async (state: void | undefined, formData: FormData) => {
         body: JSON.stringify(data),
     };
 
-    await fetchApi(routeApi("register"), options, true);
+    return fetchApi(linkApi("register.store"), options, true).then(
+        async (response) => {
+            if (!response.ok) {
+                const data = await response.json();
+
+                return {
+                    message: data.message,
+                    errors: data.errors,
+                };
+            }
+
+            await setCookies(response);
+
+            redirect(link("front.verification.notice"));
+        }
+    );
 };
