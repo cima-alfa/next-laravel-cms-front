6f02e4c46f42b519fb5b46b505db54b5498e7f54
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-01 09:35:08 +0100

    Retreive current user information

--- src/lib/data/auth.ts
+++ src/lib/data/auth.ts
@@ -1,12 +1,40 @@
 "use server";
 
-import { routeApi } from "@/lib/router/router";
+import { linkApi } from "@/lib/router/router";
 import { fetchApi } from "@/lib/utils/fetch";
 
-export const fetchUser = async (): Promise<{
+export type User = {
+    id: string;
+    username: string;
+    email: string;
+    emailVerifiedAt: string | null;
+    createdAt: string;
+    updatedAt: string;
+    nameDisplay: string;
+    nameFirst: string | null;
+    nameSecond: string | null;
+    nameLast: string | null;
+    phone: string | null;
+    phonePrefix: string | null;
+    phoneNumber: string | null;
+    locale: string;
     [key: string]: unknown;
-} | null> => {
-    return fetchApi(routeApi("api.user")).then(
-        async (response) => (await response.json())?.data ?? null
+} | null;
+
+export const fetchAuthenticated = async (): Promise<boolean> => {
+    return await fetchApi(linkApi("api.authenticated")).then(
+        async (response) => {
+            if (!response.ok) {
+                return false;
+            }
+
+            return (await response.json())?.data.authenticated;
+        }
     );
+};
+
+export const fetchUser = async (): Promise<User> => {
+    return await fetchApi(linkApi("api.user")).then(async (response) => {
+        return (await response.json())?.data ?? null;
+    });
 };
