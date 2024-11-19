7068753ac0096801b3d82436c9e2fa173e75b251
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-19 16:01:29 +0100

    Users actions

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -2,8 +2,10 @@
 
 import { hasMiddleware } from "@/lib/middleware";
 import { getCurrentRoute, link, linkApi } from "@/lib/router/router";
+import { formDataToJson } from "@/lib/utils";
 import { setCookies } from "@/lib/utils/cookies";
-import { fetchApi } from "@/lib/utils/fetch";
+import { fetchApi } from "@/lib/utils/server";
+import { revalidatePath } from "next/cache";
 import { redirect, RedirectType } from "next/navigation";
 
 export type AuthState = {
@@ -87,6 +89,136 @@ export const register = async (prevState: AuthState, formData: FormData) => {
             await setCookies(response.headers.getSetCookie());
 
             redirect(link("front.verification.notice"));
+        }
+    );
+};
+
+// export const createUser = async (prevState: PagesState, formData: FormData) => {
+//     const options: RequestInit = {
+//         method: "POST",
+//         body: toJson(formData, "title", "text"),
+//     };
+
+//     return fetchApi(linkApi("register.store"), options, true).then(
+//         async (response) => {
+//             const data = await response.json();
+
+//             if (!response.ok) {
+//                 return {
+//                     message: data.message,
+//                     errors: data.errors,
+//                 };
+//             }
+
+//             redirect(link("front.cp.pages.edit", { pageId: data.data.id }));
+//         }
+//     );
+// };
+
+export const updateUser = async (prevState: AuthState, formData: FormData) => {
+    const update = formData.get("update_user");
+    const fields: string[] = [];
+
+    switch (update) {
+        case "profile":
+            fields.push(
+                "name_first",
+                "name_second",
+                "name_last",
+                "name_display",
+                "phone",
+                "phone_prefix"
+            );
+            break;
+
+        case "email":
+            fields.push("email");
+            break;
+
+        case "username":
+            fields.push("username");
+            break;
+
+        default:
+            break;
+    }
+
+    const options: RequestInit = {
+        method: "PUT",
+        body: formDataToJson(formData, "update_user", ...fields),
+    };
+
+    return fetchApi(
+        linkApi("user-profile-information.update"),
+        options,
+        true
+    ).then(async (response) => {
+        if (!response.ok) {
+            const data = await response.json();
+
+            return {
+                message: data.message,
+                errors: data.errors,
+            };
+        }
+
+        return { message: "Successfully updated" };
+    });
+};
+
+export const updateUserPassword = async (
+    prevState: AuthState,
+    formData: FormData
+) => {
+    const options: RequestInit = {
+        method: "PUT",
+        body: formDataToJson(
+            formData,
+            "current_password",
+            "password",
+            "password_confirmation"
+        ),
+    };
+
+    return fetchApi(linkApi("user-password.update"), options, true).then(
+        async (response) => {
+            if (!response.ok) {
+                const data = await response.json();
+
+                return {
+                    message: data.message,
+                    errors: data.errors,
+                };
+            }
+
+            redirect(link("front.login"));
+        }
+    );
+};
+
+export const destroyUser = async (user: string, redirectLink?: string) => {
+    const options: RequestInit = {
+        method: "DELETE",
+    };
+
+    return fetchApi(linkApi("api.users.destroy", { user }), options, true).then(
+        async (response) => {
+            const data = await response.json();
+
+            if (!response.ok) {
+                return {
+                    message: data.message,
+                    errors: data.errors,
+                };
+            }
+
+            revalidatePath(link("front.cp.users.index"));
+
+            if (redirectLink) {
+                redirect(redirectLink, RedirectType.replace);
+            }
+
+            return { message: data.message };
         }
     );
 };
