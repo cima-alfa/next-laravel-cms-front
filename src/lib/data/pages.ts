ca920c523129905a0fe4595685da8bf71773dc54
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-11-19 16:01:03 +0100

    Fetch pages data

--- src/lib/data/pages.ts
+++ src/lib/data/pages.ts
@@ -1,13 +1,23 @@
 "use server";
 
-import { linkApi } from "@/lib/router/router";
-import { fetchApi } from "@/lib/utils/fetch";
+import { User } from "@/lib/data/auth";
+import { link, linkApi } from "@/lib/router/router";
+import { fetchApi } from "@/lib/utils/server";
+import { simulateDelay } from "@/lib/utils/server";
+import { redirect, RedirectType } from "next/navigation";
 
 export type Page = {
     id: string;
-    permalink: string;
     title: string;
     text: string;
+    user?: User | null;
+    meta: {
+        permalink: string;
+        timestamps: {
+            created_at: string;
+            updated_at: string;
+        };
+    };
     [key: string]: unknown;
 };
 
@@ -34,16 +44,29 @@ export type Pages = {
     };
 } | null;
 
-export const fetchPages = async (page?: string): Promise<Pages> => {
-    return await fetchApi(
-        linkApi("api.pages.index", { "page[number]": page ?? "1" })
-    ).then(async (response) => {
-        return (await response.json()) ?? null;
-    });
+export const fetchPages = async (
+    page = "1",
+    withUser = false
+): Promise<Pages> => {
+    await simulateDelay(1);
+
+    const params: { [key: string]: unknown } = {
+        "page[number]": page,
+    };
+
+    if (withUser) {
+        params.include = "user";
+    }
+
+    return await fetchApi(linkApi("api.pages.index", params)).then(
+        async (response) => {
+            return (await response.json()) ?? null;
+        }
+    );
 };
 
 export const fetchPageById = async (id: string): Promise<Page | null> => {
-    return await fetchApi(linkApi("api.pages.get", { id: id })).then(
+    return await fetchApi(linkApi("api.pages.get", { id })).then(
         async (response) => {
             return (await response.json())?.data ?? null;
         }
@@ -51,11 +74,17 @@ export const fetchPageById = async (id: string): Promise<Page | null> => {
 };
 
 export const fetchPageByPermalink = async (
-    permalink: string | null
+    page: string | null
 ): Promise<Page | null> => {
-    return await fetchApi(
-        linkApi("api.pages.permalink", { permalink: permalink })
-    ).then(async (response) => {
-        return (await response.json())?.data ?? null;
-    });
+    return await fetchApi(linkApi("api.pages.permalink", { page })).then(
+        async (response) => {
+            const result = await response.json();
+
+            if (result?.redirect) {
+                redirect(link(result.redirect.route), RedirectType.replace);
+            }
+
+            return result?.data ?? null;
+        }
+    );
 };
