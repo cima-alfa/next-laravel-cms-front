8b23784aedc69f5af22b5719662cc43ff44e06a0
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-12-04 13:20:13 +0100

    Wrap register form is suspense

--- src/app/(front)/(auth)/register/page.tsx
+++ src/app/(front)/(auth)/register/page.tsx
@@ -1,11 +1,13 @@
 import RegisterForm from "@/front-ui/components/app/register/RegisterForm";
+import { Suspense } from "react";
 
 export default function Page() {
     return (
         <div className="prose prose-light dark:prose-dark max-w-lg mx-auto">
             <h1>Register</h1>
-
-            <RegisterForm />
+            <Suspense>
+                <RegisterForm />
+            </Suspense>
         </div>
     );
 }
