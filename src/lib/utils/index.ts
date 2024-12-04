6074bc7eebab647dd1e3b340b6ac725e1a1a1629
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-12-04 13:20:24 +0100

    Replace lodash set with custom set

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,4 +1,4 @@
-import set from "lodash/fp/set";
+import { set } from "@/lib/utils/helpers/objects";
 
 export const sleep = async (time: number) => {
     await new Promise((resolve) => setTimeout(resolve, time * 1000));
@@ -8,7 +8,7 @@ export const formDataToObject = (formData: FormData, ...fields: string[]) => {
     let data: { [key: string]: unknown } = {};
 
     fields.forEach((field) => {
-        data = set(field, formData.get(field), data);
+        data = set(data, field, formData.get(field));
     });
 
     return data;
