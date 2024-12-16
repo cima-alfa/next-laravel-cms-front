a543d23bb08eb9cb089163f8197a222e08f0f84c
Cima Alfa <119798452+cima-alfa@users.noreply.github.com>
2024-12-16 12:09:48 +0100

    Use "formDataToJson"

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -11,6 +11,7 @@ import {
 import { setCookies } from "@cms/cookies";
 import { apiAction } from "@cms/fetch";
 import { redirect, RedirectType } from "next/navigation";
+import { formDataToJson } from "@cms/helpers";
 
 export type AuthState = {
     message: string;
@@ -38,14 +39,9 @@ export const logout = async (pathname?: string) => {
 };
 
 export const login = async (prevState: AuthState, formData: FormData) => {
-    const data = {
-        login: formData.get("login"),
-        password: formData.get("password"),
-    };
-
     const options: RequestInit = {
         method: "POST",
-        body: JSON.stringify(data),
+        body: formDataToJson(formData, "login", "password"),
     };
 
     return apiAction(linkApi("login.store"), options).then(async (response) => {
@@ -70,16 +66,15 @@ export const register = async (
     prevState: AuthState,
     formData: FormData
 ) => {
-    const data = {
-        username: formData.get("username"),
-        email: formData.get("email"),
-        password: formData.get("password"),
-        password_confirmation: formData.get("password_confirmation"),
-    };
-
     const options: RequestInit = {
         method: "POST",
-        body: JSON.stringify(data),
+        body: formDataToJson(
+            formData,
+            "username",
+            "email",
+            "password",
+            "password_confirmation"
+        ),
     };
 
     return apiAction(
@@ -108,13 +103,9 @@ export const register = async (
 };
 
 export const invite = async (prevState: AuthState, formData: FormData) => {
-    const data = {
-        email: formData.get("email"),
-    };
-
     const options: RequestInit = {
         method: "POST",
-        body: JSON.stringify(data),
+        body: formDataToJson(formData, "email"),
     };
 
     return apiAction(linkApi("api.users.invite"), options).then(
